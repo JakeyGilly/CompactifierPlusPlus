6ff1a44ca5a70ff1127ad29e87ba76396a9567ac
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-01-20 12:46:41 +0000

    chnaged makefile

---
+++ src/headers/fileio.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+std::string filetype(std::string);
+bool isFileReal(std::string);
+std::string readFile(std::string);
+int fileSize(std::string);
+void writeFile(std::string, std::string);
---
+++ src/headers/formatting.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+// GLOBAL
+std::string removeSingleAnn(std::string, std::string);
+std::string removeMultiAnn(std::string, std::string);
+
+// PYTHON
+std::string pyRemoveAnnatations(std::string);
+std::string pyFormat(std::string);
