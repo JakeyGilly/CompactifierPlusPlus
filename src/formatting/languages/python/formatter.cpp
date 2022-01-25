99111537265774af35278d1958ee4bb5a30a6f93
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-01-25 15:55:09 +0000

    YES

--- src/formatting/languages/python/formatter.cpp
+++ src/formatting/languages/python/formatter.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
 #include "../../../headers/formatting.hpp"
 
 #define PYTHON_ANNATATION "#"  // Single line comment
@@ -14,17 +15,39 @@ void pyRemoveAnnatations(std::vector<std::string> &fileContents) {
 
 void pyFormat(std::vector<std::string> &fileContents) {
 	std::cout << "Formatting Python" << std::endl;
-
-	// size_t pos;
-
-	
-	// // replace \r\n with \n
-	// fileContents.replace(std::remove(fileContents.begin(), fileContents.end(), '\r'), fileContents.end(), "\n");
-	// // replace double newline with one newline
-	// while ((pos=fileContents.find("\n\n", 0)) != -1) { fileContents.erase(pos, 1); }
-	// // remove newlines at the end of the file and at the beginning of the file
-	// if (fileContents[fileContents.length() - 1] == '\n') { fileContents.erase(fileContents.length() - 1); }
-	// if (fileContents[0] == '\n') { fileContents.erase(0, 1); }
+	// replace \r\n with \n
+	for (int i = 0; i < fileContents.size(); i++) {
+		fileContents[i].erase(std::remove(fileContents[i].begin(), fileContents[i].end(), '\r'), fileContents[i].end());
+	}
+	// remove empty lines
+	for (int i = 0; i < fileContents.size(); i++) {
+		if (fileContents[i].empty()) {
+			fileContents.erase(fileContents.begin() + i);
+			i--;
+		}
+	}
+	// remove newlines at the end of the file and at the beginning of the file
+	if (fileContents.size() > 0) {
+		if (fileContents[0].empty()) {
+			fileContents.erase(fileContents.begin());
+		}
+		if (fileContents[fileContents.size() - 1].empty()) {
+			fileContents.erase(fileContents.end() - 1);
+		}
+	}
+	bool removeline = true;
+	for (int i = 0; i < fileContents.size(); i++) {
+		for (int j = 0; j < fileContents[i].length(); j++) {
+			if (fileContents[i][j] != ' ' && fileContents[i][j] != '\t') {
+				removeline = false;
+			} 
+		}
+		if (removeline) {
+			fileContents.erase(fileContents.begin() + i);
+			i--;
+		}
+		removeline = true;
+	}
 	// // remove multiple spaces after text and before newline
 	// // remove blank lines
 	// while ((pos=fileContents.find("\n\n", 0)) != -1) { fileContents.erase(pos, 1); }
